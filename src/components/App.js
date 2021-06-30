8b4a98b28452fa03796250a903e4efb37f5d0bd7
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-06-30 17:37:11 -0400

    Se cambian archivo App.js

--- src/components/App.js
+++ src/components/App.js
@@ -28,87 +28,30 @@
 
 
 
-const App = (data,dataJS) => {
-  let dataObject=Object.entries(dataJS);
-  let backCardObject=Object.values(dataObject[2]);
-  const containerCards=document.createElement('section');
-  containerCards.className='container-grid';
-  let click=[];
-let flipped=[];
-let cartasGiradas=[];
-let delay=1200;
+const App = (data) => {
+  const grid=document.createElement('div');
+  data.forEach(item => {
+    const item={ id, image } ;
   
-const cardCliked=(cardSet)=>{
-  for (let i=0 ;i<cardSet.length;i++){
-       cardSet[i].classList.toggle('flipped');
-       cartasGiradas.push(cardSet[i]);
-       }
-       console.log(cardSet);
-       //cardSet[1].classList.toggle('flipped');
-
- 
-};
-
-const flippCard=(target,idTarjet)=>{
-  if(click.length<2 && !click.includes(target)){
-    click.push(document.getElementById(idTarjet));
-    target.classList.toggle('flipped');
-    console.log(click);
-       if(click.length===2){
-        setTimeout(cardCliked(click),delay);
-       }
-
-    }
-};
-const change=(event)=>{
-  let idCard=event.currentTarget.id;
-  event.currentTarget.classList.toggle('select');
-  flippCard(event.currentTarget,idCard);
-  console.log(cartasGiradas);
-  console.log(idCard);
-};
-    
-
-
-  data.forEach((key,id)=>{
-    const card=document.createElement('div');
-    card.className='card';
-    const frontCard=document.createElement('div');
-    const imageFrontCard=document.createElement('img');
-    frontCard.className='frontCard';
-    frontCard.setAttribute('id','frontCard'+id);
-    card.setAttribute('id',id);
-    imageFrontCard.setAttribute('id',"image"+ id);
-    imageFrontCard.className='image';
-    imageFrontCard.src=key;
-    const backCard=document.createElement('div');
-    const imageBackCard=document.createElement('img');
-    backCard.className='backCard';
-    backCard.setAttribute('id','backCard'+id);
-    imageBackCard.src=backCardObject[1];
-    imageBackCard.className = 'imageBackCard';
-    frontCard.appendChild(imageFrontCard);
-    backCard.appendChild(imageBackCard);
-    card.appendChild(frontCard);
-    card.appendChild(backCard);
-    containerCards.appendChild(card);
-    card.addEventListener('click',change);   
-}); 
-  return containerCards;
-};
-
-    
- 
-
-   /* if(click.length===2){
-      event.currentTarget.classList.toggle('flipped');
-    }*/
-    //sevent.currentTarget.classList.toggle('flipped');
-   /* if(click.length===2){
-       event.target.parentElement.classList.toggle('flipped');
-    }*/
-    //}
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.name = item.id;
   
+    const front = document.createElement('div');
+    front.classList.add('front');
+  
+    const back = document.createElement('div');
+    back.classList.add('back');
+    back.style.backgroundImage = `url(${item.image})`;
+  
+    grid.appendChild(card);
+    card.appendChild(front);
+    card.appendChild(back);
+  });
+  
+  return grid;
+};
+
     
 
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import App from './components/App.js';
+import setData from './components/Setdata.js';
+import shuffle from  './components/Shufflecards.js';
+import pokemon from   '../data/pokemon/pokemon.js';
 
-document.getElementById('root').appendChild(App());
+
+document.getElementById('root').appendChild(App(shuffle(setData(pokemon))));
