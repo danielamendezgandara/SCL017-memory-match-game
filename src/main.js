3ef578e70e9c1946e0ee688f03c66a4512623eff
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-07-02 16:18:14 -0400

    Se modifica archivo main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import App from './components/App.js';
+import mariobros from '../data/mariobros/mariobros.js';
 import setData from './components/Setdata.js';
-import shuffle from  './components/Shufflecards.js';
-import pokemon from   '../data/pokemon/pokemon.js';
+import shuffle from './components/Shufflecards.js';
+import createHeader from './components/Headergame.js';
+//import displayGame from './components/DisplayGame.js';
+//import matchGame from './components/Matchgame.js';
+/*fetch('./data/pokemon/pokemon.json')
+  .then(resp => resp.json())
+  .then(console.log)
+  .catch(console.error);*/
+document.getElementById('root').appendChild(createHeader());
+//document.getElementById('root').appendChild(displayGame());
+document.getElementById('root').appendChild(App (shuffle(setData(mariobros),6),mariobros));
 
-
-document.getElementById('root').appendChild(App(shuffle(setData(pokemon))));
