1a2b4ca4318cd72fc0ade38816091a9ac38d2678
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-07-02 16:50:20 -0400

    Se agrega audios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import App from './components/App.js';
 import mariobros from '../data/mariobros/mariobros.js';
 import setData from './components/Setdata.js';
 import shuffle from './components/Shufflecards.js';
-import createHeader from './components/Headergame.js';
+import createHeader from './components/HeaderGame.js';
 //import displayGame from './components/DisplayGame.js';
 //import matchGame from './components/Matchgame.js';
 /*fetch('./data/pokemon/pokemon.json')
