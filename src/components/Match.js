4b5cb84c1cff974f3cc19fd0b5d440bb06eb4871
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-07-06 17:32:41 -0400

    Se crea archivo Match.js

---
+++ src/components/Match.js
@@ -0,0 +1,8 @@
+const match = () => {
+    const selected = document.querySelectorAll('.flipped');
+    selected.forEach(card => {
+      card.classList.add('match');
+     });
+  };
+
+export default match;
