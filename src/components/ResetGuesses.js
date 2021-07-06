e3aaf8fb8a3d7b323b415fa70364030cab4e05c7
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-07-06 17:35:38 -0400

    Se crea función resetguesses

---
+++ src/components/ResetGuesses.js
@@ -0,0 +1,8 @@
+const resetGuesses = () => {
+    let selected = document.querySelectorAll('.flipped');
+    selected.forEach(card => {
+      card.classList.remove('flipped');
+    });
+    };
+
+export default resetGuesses;
