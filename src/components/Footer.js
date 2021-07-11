023d883ee645f78b0227ebf2a78bce1048a72374
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-07-11 15:48:47 -0400

    Se aplican cambios de estilo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,7 @@ const createFooter=()=>{
 
 const footer = document.createElement('footer')
 footer.textContent=  "© 2021 Laboratoria SCL017 Daniela Mendez & Maria Parra";
-footer.className='footer'
-//footer.className = 'footer';
+footer.className='footer';
 
 return  footer;
 
