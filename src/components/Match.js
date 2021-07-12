e4a09e903ead35542500aeac4e28a41a3d8df163
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-07-12 13:15:21 -0400

    Se borran archivos

--- src/components/Match.js
+++
@@ -1,7 +0,0 @@
-const match = () => {
-    const selected = document.querySelectorAll('.flipped');
-    selected.forEach(card => {
-      card.classList.add('match');
-    });
-    };
-    export default match;
--- src/components/ResetGuesses.js
+++
@@ -1,9 +0,0 @@
-const resetGuesses = () => {
-
-
-    let selected = document.querySelectorAll('.flipped');
-    selected.forEach(card => {
-      card.classList.remove('flipped');
-    });
-    };
-    export default resetGuesses;
